688ab383e775af5242b0f4e3f32bd33ad6cc0d3e
Yoann Gini <yoann.gini@gmail.com>
2016-02-28 16:14:04 +0100

    Improve notifiction management to apply settings only once.

--- src/main.swift
+++ src/main.swift
@@ -7,53 +7,62 @@ import Cocoa
 var run = true
 let preferenceDomain = "com.abelionni.DockServiceManager"
 let defaults = NSUserDefaults(suiteName: preferenceDomain)
-
+var previousDockitems = NSArray()
 
 
 func handleSettingsUpdates() {
-    print("Applying settings from " + preferenceDomain)
     
-    for item in (defaults?.arrayForKey("dockitems") as? [[String:String]])! {
-        var args = [String]()
+    if let dockitems = defaults?.arrayForKey("dockitems") as? [[String:String]] {
         
-        for key in item.keys {
-            var potentialValue = item[key]
+        if !previousDockitems.isEqualToArray(dockitems) {
             
-            if var value = potentialValue {
-                if key == "add" {
-                    if !value.containsString("/")
-                        && value.containsString(".") {
-                            potentialValue = NSWorkspace.sharedWorkspace().absolutePathForAppBundleWithIdentifier(value)
-                            if (potentialValue != nil) {
-                                value = potentialValue!
+            print("Applying settings from " + preferenceDomain)
+            
+            for item in dockitems {
+                var args = [String]()
+                
+                for key in item.keys {
+                    var potentialValue = item[key]
+                    
+                    if var value = potentialValue {
+                        if key == "add" {
+                            if !value.containsString("/")
+                                && value.containsString(".") {
+                                    potentialValue = NSWorkspace.sharedWorkspace().absolutePathForAppBundleWithIdentifier(value)
+                                    if (potentialValue != nil) {
+                                        value = potentialValue!
+                                    }
                             }
+                        }
+                        
+                        args.append("--"+key)
+                        args.append(value)
                     }
                 }
                 
-                args.append("--"+key)
-                args.append(value)
+                print("Running dockutil with ", args)
+                let dockutil = NSTask.launchedTaskWithLaunchPath("/usr/local/DockServiceManager/dockutil", arguments: args)
+                
+                dockutil.waitUntilExit()
             }
+            
+            previousDockitems = dockitems
+        } else {
+            print("Settings already applied during this run, skipping notification.")
         }
-        
-        print("Running dockutil with ", args)
-        let dockutil = NSTask.launchedTaskWithLaunchPath("/usr/local/DockServiceManager/dockutil", arguments: args)
-        
-        dockutil.waitUntilExit()
     }
 }
 
 NSDistributedNotificationCenter.defaultCenter().addObserverForName("com.apple.MCX._managementStatusChangedForDomains", object: "com.apple.MCX", queue: nil) { (note) -> Void in
     
-    let potentialDomains = note.userInfo!["com.apple.MCX.changedDomains"] as? [String]
-    
-    if let domains = potentialDomains {
-        if domains.contains("com.abelionni.DockServiceManager") {
-            handleSettingsUpdates()
+    if let userInfo = note.userInfo {
+        if let domains = userInfo["com.apple.MCX.changedDomains"] as? [String] {
+            if domains.contains("com.abelionni.DockServiceManager") {
+                handleSettingsUpdates()
+            }
         }
     }
 }
-
-//NSNotificationCenter.defaultCenter().postNotificationName(NSUserDefaultsDidChangeNotification, object: nil)
 
 let runLoop = NSRunLoop.currentRunLoop()
 runLoop.run()
