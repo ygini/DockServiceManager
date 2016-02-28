20d030f822a1fd82ae0b05d5c10861da2b1e16b7
Yoann Gini <yoann.gini@gmail.com>
2016-02-28 16:14:13 +0100

    package update for beta 0.3

--- src/postinstall.sh
+++ src/postinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mv /usr/local/DockServiceManager/main.swift /usr/local/DockServiceManager/DockServiceManager.swift
+ln -s /usr/local/DockServiceManager/main.swift /usr/local/DockServiceManager/DockServiceManager.swift
 
 exit 0
